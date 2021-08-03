f51e40de67dcbc85c7d44dacf7d1e0b2ecced96e
Krzysztof Kozlowski <krzysztof.kozlowski@pl.whoohoo.com>
2021-08-03 11:12:10 +0200

    Add UTs

---
+++ tests/Battleship.Tests/PlayerTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Battleship.Core.Domain.Entities;
+using Battleship.Core.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void Player_AddShipInGrid()
+        {
+            var player = CreateValidPlayer();
+            Ship ship = new Destroyer(ShipDirection.Horizontal, 0, 0);
+            
+            player.AddShip(ship);
+        }
+
+        [Fact]
+        public void Player_OnAddShipNotInGridThrowException()
+        {
+            var player = CreateValidPlayer();
+            Ship ship = new Cruiser(ShipDirection.Horizontal, 9, 9);
+            
+            Action act = () =>  player.AddShip(ship);
+
+            act.Should().ThrowExactly<PlayerFailedToAddShipException>();
+        }
+
+        [Fact]
+        public void Player_ShootOnTarget()
+        {
+            var player = CreateValidPlayer();
+            Ship ship = new Cruiser(ShipDirection.Horizontal, 0, 0);
+            player.AddShip(ship);
+
+            player.Shoot(0, 0);
+
+            var square = player.GetSquares()[0][0];
+
+            square.Should().Be(SquareState.OnTarget);
+        }
+
+        [Fact]
+        public void Player_MissedShot()
+        {
+            var player = CreateValidPlayer();
+            Ship ship = new Cruiser(ShipDirection.Horizontal, 0, 0);
+            player.AddShip(ship);
+
+            player.Shoot(5, 5);
+
+            var square = player.GetSquares()[5][5];
+
+            square.Should().Be(SquareState.Missed);
+        }
+        
+        private Player CreateValidPlayer()
+        {
+            return new Player(Guid.NewGuid(), "Player name", new Grid(10, 10));
+        }
+    }
+}
---
+++ tests/Battleship.Tests/ShipTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Battleship.Core.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests
+{
+    public class ShipTests
+    {
+        [Fact]
+        public void ShipTests_GetCoordinatesForHorizontalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Horizontal, 0, 0);
+
+            var coordinates = ship.GetCoordaintes();
+            var expectedCoordinates = new List<(int, int)>
+            {
+                (0, 0),
+                (1, 0)
+            };
+
+            coordinates.Should().Equal(expectedCoordinates);
+        }
+        
+        [Fact]
+        public void ShipTests_GetCoordinatesForVerticalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Vertical, 0, 0);
+
+            var coordinates = ship.GetCoordaintes();
+            var expectedCoordinates = new List<(int, int)>
+            {
+                (0, 0),
+                (0, 1)
+            };
+
+            coordinates.Should().Equal(expectedCoordinates);
+        }
+        
+        [Fact]
+        public void ShipTests_GetMaxCoordinatesForVerticalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Vertical, 0, 0);
+
+            var coordinates = ship.GetMaxCoordinates();
+            var expectedCoordinates = (0, 0);
+
+            coordinates.Should().Be(expectedCoordinates);
+        }
+        
+        [Fact]
+        public void ShipTests_GetMinCoordinatesForVerticalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Vertical, 0, 0);
+
+            var coordinates = ship.GetMinCoordinates();
+            var expectedCoordinates = (0, 1);
+
+            coordinates.Should().Be(expectedCoordinates);
+        }
+        
+        [Fact]
+        public void ShipTests_GetMaxCoordinatesForHorizontalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Horizontal, 0, 0);
+
+            var coordinates = ship.GetMaxCoordinates();
+            var expectedCoordinates = (0, 0);
+
+            coordinates.Should().Be(expectedCoordinates);
+        }
+        
+        [Fact]
+        public void ShipTests_GetMinCoordinatesForHorizontalDestroyer()
+        {
+            Ship ship = new Destroyer(ShipDirection.Horizontal, 0, 0);
+
+            var coordinates = ship.GetMinCoordinates();
+            var expectedCoordinates = (1, 0);
+
+            coordinates.Should().Be(expectedCoordinates);
+        }
+    }
+}
